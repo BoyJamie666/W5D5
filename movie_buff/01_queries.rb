0977a2a2048cc52d15f43bdd52f8aec42087e80c
Jamie <jamesmaclay@gmail.com>
2023-09-22 14:05:44 -0400

    harrison ford

--- movie_buff/01_queries.rb
+++ movie_buff/01_queries.rb
@@ -6,6 +6,9 @@ def it_was_ok
   # You can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3.
+  Movie
+    .select("id, title, score")
+    .where("score BETWEEN 2 AND 3")
 
 end
 
@@ -20,6 +23,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford appeared but not
   # as a lead actor.
+  Actor
+    .joins(:movies)
+    .select("movies.id, movies.title")
+    
+    .where(acto)
 
 end
 
